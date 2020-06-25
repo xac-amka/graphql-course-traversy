b8e617a55437314d7f335167be024d24b82b747b
Proboss <64521451+xac-amka@users.noreply.github.com>
2020-06-25 19:27:25 +0800

    update

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use('/graphql', expressGraphQL({
     graphiql: true
 }));
 
+app.get('/', (req, res) =>{
+    res.send('<h1>This is GraphQL example</h1><a href="/graphql">Jump to GraphQL</a>')
+});
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {console.log('App is running on port ' + PORT)});
